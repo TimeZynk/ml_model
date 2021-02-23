9346223fca848a3208f13daa7f7374be5aa4f4ae
Chuck Chen <chuck@timezynk.com>
2021-02-23 11:12:40 +0100

    Modified app.py

--- src/app.py
+++ src/app.py
@@ -2,13 +2,11 @@ from knn.manager import ModelManager
 import os
 import logging
 from datetime import datetime
-from debounce import debounce
 import time
 
 logging.basicConfig(format="%(asctime)-15s %(message)s", level=logging.INFO)
 
 
-@debounce(1)
 def run():
     logger = logging.getLogger(__name__)
     if "MODELS_DIR" not in os.environ or "MONGO_URI" not in os.environ:
@@ -26,14 +24,18 @@ def run():
         logger.info("ML models trained.")
 
 
-if __name__ == "__main__":
-
+def main():
+    runs_at = os.getenv("RUNS_AT", "").strip()
     if bool(os.getenv("DEV_ENV")):
         run()
-    while True and not bool(os.getenv("DEV_ENV")):
-        cur_time = time.strftime("%H:%M:%S")
-        runs_at = (
-            os.getenv("RUNS_AT") if os.getenv("RUNS_AT") is not None else "02:00:00"
-        )
-        if cur_time == os.getenv("RUNS_AT"):
-            run()
+    else:
+        while bool(runs_at):
+            cur_time = time.strftime("%H:%M")
+
+            if cur_time == runs_at:
+                run()
+            time.sleep(30)
+
+
+if __name__ == "__main__":
+    main()
